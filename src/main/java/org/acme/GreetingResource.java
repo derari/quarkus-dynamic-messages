09789e1da8145a05fa6bf020aa0f99ba66e6232a
Arian Treffer <arian.treffer@easybell.de>
2024-06-12 12:48:47 +0200

    initial

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,48 @@
 package org.acme;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.i18n.Localized;
+import io.quarkus.qute.i18n.MessageBundles;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestPath;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    Template page;
+
+    @Inject
+    DynamicTemplates dynamicTemplates;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{locale}/{name}")
+    public TemplateInstance hello(@RestPath String locale, @RestPath String name) {
+        return page.data("name", name).setLocale(locale);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Path("{locale}")
+    public String ok(@RestPath String locale) {
+        try {
+            return MessageBundles.get(MyMessages.class, Localized.Literal.of(locale)).ok();
+        } catch (IllegalStateException ex) {
+            return MessageBundles.get(MyMessages.class).ok();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("set/{locale}/{key}/{value}")
+    public String set(@RestPath String locale, @RestPath String key, @RestPath String value) {
+        dynamicTemplates.update(locale, key, value);
+        return value;
     }
 }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -5,16 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/hello/en/Bob")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(containsString("Bob"));
     }
 
 }
